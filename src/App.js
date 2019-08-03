e5113b1781f713e597779b6df0ab299ddb857b86
Wanwan <wanwanzainaer@gmail.com>
2019-08-02 17:04:13 -0700

    Add some sample for the web mockup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
+
+//next step add react router
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      For the first init
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Dashboard />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
+
+export default Footer;
