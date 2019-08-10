e3b6957833ce1b566ba65bf57da0d77047074952
Wanwan <wanwanzainaer@gmail.com>
2019-08-09 20:01:54 -0700

    Updates

---
+++ precache-manifest.cf4171838c880d74b10335c1fe678252.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0afcf4087feed00b0a4793fc2709325",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5198a194f5c148dcf14c",
+    "url": "/portfolio/static/css/2.e63ab5f0.chunk.css"
+  },
+  {
+    "revision": "6b5a7f442e43e8e41e58",
+    "url": "/portfolio/static/css/main.baa26b4b.chunk.css"
+  },
+  {
+    "revision": "5198a194f5c148dcf14c",
+    "url": "/portfolio/static/js/2.cf9d4943.chunk.js"
+  },
+  {
+    "revision": "6b5a7f442e43e8e41e58",
+    "url": "/portfolio/static/js/main.cfd4d53c.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/portfolio/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/portfolio/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/portfolio/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/portfolio/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "e850063f3abd056d9448869693be4a8e",
+    "url": "/portfolio/static/media/Me.e850063f.jpg"
+  },
+  {
+    "revision": "d07341e7cf30d93d9eb83920d1dba3c8",
+    "url": "/portfolio/static/media/file017132.d07341e7.jpg"
+  },
+  {
+    "revision": "1e40745db9cc8da2be957ecd81a0ed28",
+    "url": "/portfolio/static/media/flashimage1.1e40745d.JPG"
+  },
+  {
+    "revision": "a8f341a8db852208966ca4b03ff180d7",
+    "url": "/portfolio/static/media/flashimage2.a8f341a8.JPG"
+  },
+  {
+    "revision": "d224251ea6cb3e32d90964c1373bb10f",
+    "url": "/portfolio/static/media/flashimage3.d224251e.JPG"
+  },
+  {
+    "revision": "f9cefe90f4703840d6a4eaf7c533f0a1",
+    "url": "/portfolio/static/media/flashimage4.f9cefe90.JPG"
+  },
+  {
+    "revision": "979764085c7dc4889d333a258f9864fc",
+    "url": "/portfolio/static/media/flashimage5.97976408.JPG"
+  }
+]);
